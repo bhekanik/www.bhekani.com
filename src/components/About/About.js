f996fd30fe43dade8a84677dd2660b0dff1159e3
Bhekani Khumalo <bhekanik@gmail.com>
2019-04-19 02:21:56 +0200

    edited text on about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -30,15 +30,15 @@ const About = () => {
         />
         <h1 className="about-title">About me</h1>
         <h2 className="about-subtitle">
-          {"Software Developer, Civil Engineer & GIS Specialist"}
+          {"Software Developer, GIS Specialist & Civil Engineer."}
         </h2>
         <div className="about-text">
           <p>
             {
-              "I am a Software Developer with a background as a Civil Engineer and Geographic Information Systems (GIS) Specialist. I have experience in design, development and project management within the municipal engineering and consulting environments. I have traceable work in GIS, software development and engineering – including project management, data collection, analysis, and publishing. I recently wrote an "
+              "I am a Software Developer with a background as a Civil Engineer and Geographic Information Systems (GIS). I have traceable work in GIS, software development and engineering – including project management, data collection, analysis, and publishing. I recently wrote an "
             }
             <a href="https://www.offerzen.com/blog/what-civil-engineering-taught-me-about-being-a-good-software-developer">
-              article for OfferZen
+              article
             </a>
             {
               "on what Civil Engineering taught me about being a good software developer."
@@ -46,35 +46,44 @@ const About = () => {
           </p>
           <p>
             {
-              "I am very passionate about learning so I have no religious loyalty to any technology but I do prefer full stack web development. I am proficient in JavaScript, NodeJS, Python, React and HTML/CSS and I'm currently expanding my skills in ASP.NET and C#. I really love working with GIS or anything dealing with geo-spatial technologies because it offers the opportunity to interact many kinds of problems from different fields. Because of that love, I have worked as a consultant on for the City of Harare, Zimbabwe in establishing their GIS department during the "
+              "I am very passionate about learning so I have no big loyalty to any technology. However, I am proficient in JavaScript, NodeJS, Python, React, React Native and HTML/CSS and I'm currently expanding my skills in ASP.NET and C#. "
             }
-            <a href="https://www.giz.de/en/html/index.html">GIZ</a> supervised{" "}
+          </p>
+              
+          <p> 
+            {
+              " I really love working with geo-spatial technologies because it offers the opportunity to interact with many kinds of problems from different fields. Because of that love, I have worked as a consultant on for the City of Harare, Zimbabwe in establishing their GIS department during the "
+              }
+            <a href="https://www.giz.de/en/html/index.html">GIZ</a>
+            {" supervised "}
             <a href="https://www.muenchen.de/rathaus/Stadtpolitik/Partnerstaedte/Harare/Staedtische_Projekte/kooperationsprojekt.html">
               ZIGESA Trialogue
-            </a>{" "}
-            in 2015 which was later extended under{" "}
+            </a>
+            {" in 2015 which was later extended under "}
             <a href="https://skew.engagement-global.de/funding-from-the-nakopa-programme.html">
               Nakopa
-            </a>{" "}
-            funding in 2015 - 2016. I also gave a talk titled Water Demand
-            Analysis using GIS at the 2015 ESRI Africa User Conference in Cape
-            Town, South Africa based on some work I had done as a water engineer
-            at the <a href="http://citybyo.co.zw/">City of Bulawayo</a> .
+            </a>
+            {
+              " funding in 2015 - 2016. I also gave a talk titled Water Demand Analysis using GIS at the 2015 ESRI Africa User Conference in Cape Town, South Africa based on some work I had done as a water engineer at the "
+            }
+            <a href="http://citybyo.co.zw/">City of Bulawayo.</a>
           </p>
           <p>
-            Besides software development, engineering and GIS. I am a sketch
-            artist and painter, you can find some of my work on{" "}
-            <a href="https://www.deviantart.com/bhekanik">DeviantArt</a>. I am
-            also a co-host of the{" "}
+            {
+              "Besides software development, engineering and GIS. I am a sketch artist and painter, you can find some of my work on "
+            }
+            <a href="https://www.deviantart.com/bhekanik">DeviantArt</a>
+            {". I am also a co-host of the "}
             <a href="https://anchor.fm/justlivinlife">
               Just Livin' Life podcast
-            </a>{" "}
-            and the{" "}
+            </a>
+            {" and the "}
             <a href="https://www.youtube.com/channel/UCNNaw5_xYuNle7jUU5Y8mmQ">
               Just Livin' Life YouTube channel
-            </a>{" "}
-            where my fiancee and I talk about all things relationships along
-            with other topics that interest us.
+            </a>
+            {
+              " where my fiancee and I talk about all things relationships along with other topics that interest us."
+            }
           </p>
         </div>
       </section>
