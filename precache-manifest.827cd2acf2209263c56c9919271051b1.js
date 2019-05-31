2c7f6db8e296f465a17765572e5d7689e81344ab
Bhekani Khumalo <bkhumalo@esri-southafrica.com>
2019-05-31 10:13:42 +0200

    Updates

---
+++ precache-manifest.827cd2acf2209263c56c9919271051b1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cf816c57e9e8bf34ef9",
+    "url": "/react.bhekani.com/static/css/main.ce7672ba.chunk.css"
+  },
+  {
+    "revision": "1cf816c57e9e8bf34ef9",
+    "url": "/react.bhekani.com/static/js/main.1e3215a2.chunk.js"
+  },
+  {
+    "revision": "3f97131d676a4f3b5d77",
+    "url": "/react.bhekani.com/static/js/runtime~main.83fa7a81.js"
+  },
+  {
+    "revision": "411330168f635e74361b",
+    "url": "/react.bhekani.com/static/js/2.b01916ff.chunk.js"
+  },
+  {
+    "revision": "a4d9f86ab48cc887a3ce",
+    "url": "/react.bhekani.com/static/css/3.39a0ec18.chunk.css"
+  },
+  {
+    "revision": "a4d9f86ab48cc887a3ce",
+    "url": "/react.bhekani.com/static/js/3.f2b01144.chunk.js"
+  },
+  {
+    "revision": "e06fab6d2f3fc31039e94ec6b8a401f5",
+    "url": "/react.bhekani.com/static/media/intro-left.e06fab6d.jpg"
+  },
+  {
+    "revision": "aa60b5761384330ff86b031a4fe51ff3",
+    "url": "/react.bhekani.com/static/media/intro-right.aa60b576.jpg"
+  },
+  {
+    "revision": "7524d0c05a40bef02e96177c0e36210b",
+    "url": "/react.bhekani.com/static/media/arrow.7524d0c0.svg"
+  },
+  {
+    "revision": "83f0477b8037417efabc185ef32b0ce3",
+    "url": "/react.bhekani.com/static/media/css_logo.83f0477b.png"
+  },
+  {
+    "revision": "4c48053ed95e28a2ec7c6e3b784b1c16",
+    "url": "/react.bhekani.com/static/media/js_logo.4c48053e.png"
+  },
+  {
+    "revision": "6d454ae448825df4cd80fb76bd0bbd6a",
+    "url": "/react.bhekani.com/static/media/sass_logo.6d454ae4.png"
+  },
+  {
+    "revision": "b9bbb1d722d3ef6ebf7d573703b5925e",
+    "url": "/react.bhekani.com/static/media/python_logo.b9bbb1d7.png"
+  },
+  {
+    "revision": "66657f5dc34703daceb62cb80cf2f7d4",
+    "url": "/react.bhekani.com/static/media/react_logo.66657f5d.png"
+  },
+  {
+    "revision": "206148fa32b59cf02320e19aeac453da",
+    "url": "/react.bhekani.com/static/media/reactnative_logo.206148fa.png"
+  },
+  {
+    "revision": "b819d940dbd89a1d3f0869538ff32312",
+    "url": "/react.bhekani.com/static/media/arcgis_logo.b819d940.png"
+  },
+  {
+    "revision": "9e3341310838f996e28ac9046f85a0ab",
+    "url": "/react.bhekani.com/static/media/git_logo.9e334131.png"
+  },
+  {
+    "revision": "3d19ec92b0499d2c0a923a6d95674a21",
+    "url": "/react.bhekani.com/static/media/ol_logo.3d19ec92.png"
+  },
+  {
+    "revision": "e0e8edcb9cdda39d673dc76dfa35924a",
+    "url": "/react.bhekani.com/index.html"
+  }
+];
